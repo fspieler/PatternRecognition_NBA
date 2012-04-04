8333496aff72d65f970573199cc55a7477603146
Fred Spieler <fspieler@gmail.com>
2012-04-04 18:21:09 -0300

    Fixed order for raw_data indexes.

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ clear all
 
 %%load data from csvs into raw_data
 
-    raw_data = zeros(2012-2004,30,35,82);
+    raw_data = zeros(2012-2004,30,82, 35);
 
     for year=2005:1:2012
         S = urlread([ 'file:///U:\My Documents\MATLAB\fspieler-PatternRecognition_NBA-b719940\'...
@@ -15,7 +15,7 @@ clear all
         [m n] = size(C{1}(:));
         for i = 1:m
             for j = 1:35
-                raw_data(year-2004, char(C{1}(i)), j,(C{2}(i))) = C{j}(i);
+                raw_data(year-2004, char(C{1}(i)), (C{2}(i)), j) = C{j}(i);
             end
         end
     end
